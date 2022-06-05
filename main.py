113c98f644648530ef02f79c8ec9ba24bc5012df
Joonhyuk Ko <joonhyukko@Joons-MacBook-Air.local>
2022-06-05 03:14:22 -0400

    third commit

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-print('hello world')
+import numpy as np
+import matplotlib as plt
+import csv
+#from sklearn.datasets import
+import pandas as pd
 
-list = ["This", "is", "Joon"]
-for i in list:
-    print(i)
+def main():
+    print("hello world")
+
+    data = pd.read_csv("dataSet/train.csv")
+
+    #with open('dataSettrain.csv', 'r') as file:
+        #my_reader = csv.reader(file, delimiter=',')
+        #for row in my_reader:
+            #print(row)
+    
+if __name__ == "__main__":
+    main()
